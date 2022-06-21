9ea143a8a5815b205d3bf37bd8031310f5483bf7
FooOperator <foxoperator1@gmail.com>
2022-06-21 14:39:42 +0100

    better styling sass implementation removed filters due to dataset limitation

---
+++ pages/pokemon/[id].tsx
@@ -0,0 +1,82 @@
+/* eslint-disable @next/next/no-img-element */
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from '../../styles/pokemon[id].module.scss';
+import { Pokemon } from "../../types/pokemon";
+
+export default function Details() {
+    const {
+        query: { id }
+    } = useRouter();
+
+    const [currentPokemon, setCurrentPokemon] = useState<Pokemon>();
+
+    useEffect(() => {
+        async function getOnePokemon() {
+            const response = await fetch(`https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`);
+            const data = await response.json();
+            console.log('this is raw data', data);
+            setCurrentPokemon(data);
+        }
+
+        if (id) {
+            getOnePokemon();
+        }
+    }, [id]);
+
+    if (!currentPokemon) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className={styles["container"]}>
+            <Head>
+                <title>{currentPokemon?.name ?? 'Loading...'}</title>
+            </Head>
+            <div>
+                <Link href={'/'}>
+                    <a>
+                        <button className={styles['back-button']}>
+                            Back
+                        </button>
+                    </a>
+                </Link>
+            </div>
+            <div className={styles['content']}>
+                <img
+                    src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${currentPokemon.image}`}
+                    alt={currentPokemon.name}
+                    className={styles['pokemon-image']}
+                />
+                <div className={styles['description-column']}>
+                    <div>
+                        <h1>{currentPokemon.name}</h1>
+                        <h2>{currentPokemon.type.slice(0, 2).join('/')}</h2>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                currentPokemon.stats.map((stat, index) =>
+                                    <tr
+                                        key={index}
+                                        className={styles['stat-row']}
+                                    >
+                                        <td className={styles['name-data']}>{stat.name}</td>
+                                        <td className={styles['value-data']}>{stat.value}</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ types/pokemon.ts
@@ -0,0 +1,18 @@
+export const POKEMON_TYPES = <const>['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy'];
+
+export type PokemonType = typeof POKEMON_TYPES[number];
+
+export type Pokemon = {
+    id: string;
+    name: string;
+    image: string;
+    stats: [
+        { name: 'HP', value: number },
+        { name: 'Attack', value: number },
+        { name: 'Defense', value: number },
+        { name: 'Sp. Attack', value: number },
+        { name: 'Sp. Defense', value: number },
+        { name: 'Speed', value: number }
+    ];
+    type: PokemonType[];
+}
